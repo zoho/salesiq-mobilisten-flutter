60edfd3e627f7bbfc38ba8ed0b887813df69bb4d
Rishabh Raghunath <rishabh.r@zohocorp.com>
2021-10-01 19:05:32 +0530

    Release 1.0.0

--- lib/mobilisten_plugin.dart
+++ lib/mobilisten_plugin.dart
@@ -10,13 +10,19 @@ class ZohoSalesIQ {
   static const String _mobilistenArticleEventChannel =
       "mobilistenFAQEventChannel";
 
+  /// Stream to receive general mobilisten events.
   static final eventChannel =
       EventChannel(_mobilistenEventChannel).receiveBroadcastStream();
+
+  /// Stream to receive mobilisten events related to chat.
   static final chatEventChannel =
       EventChannel(_mobilistenChatEventChannel).receiveBroadcastStream();
+
+  /// Stream to receive events related to the knowledge base.
   static final articleEventChannel =
       EventChannel(_mobilistenArticleEventChannel).receiveBroadcastStream();
 
+  /// Initializes Mobilisten using the [appKey] and [accessKey] generated for the bundle ID/package name of an application.
   static Future<Null> init(String appKey, String accessKey) async {
     Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent("appKey", () => appKey);
@@ -24,102 +30,132 @@ class ZohoSalesIQ {
     await _channel.invokeMethod('init', args);
   }
 
+  /// Controls the visibility of the default launcher using the value provided for [show].
   static Future<Null> showLauncher(bool show) async {
     await _channel.invokeMethod('showLauncher', show);
   }
 
+  /// Sets the language used by Mobilisten using the language code provided in [language].
   static Future<Null> setLanguage(String language) async {
     await _channel.invokeMethod('setLanguage', language);
   }
 
+  /// Sets the department to which all chat requests are routed by default.
   static Future<Null> setDepartment(String department) async {
     await _channel.invokeMethod('setDepartment', department);
   }
 
+  /// Sets the list of departments to which chat requests may be routed.
   static Future<Null> setDepartments(List<String> departmentList) async {
     await _channel.invokeMethod('setDepartments', departmentList);
   }
 
+  /// Prefills the text provided as [question] in the chat input field for a new chat window.
   static Future<Null> setQuestion(String question) async {
     await _channel.invokeMethod('setQuestion', question);
   }
 
+  /// Automatically attempts to starts a chat using the text provided in [question] as the question.
   static Future<Null> startChat(String question) async {
     await _channel.invokeMethod('startChat', question);
   }
 
+  /// Enables or disables conversation history using the value provided for [visibility].
   static Future<Null> setConversationVisibility(bool visibility) async {
     await _channel.invokeMethod('setConversationVisibility', visibility);
   }
 
+  /// Sets the title for the conversations list.
   static Future<Null> setConversationListTitle(String title) async {
     await _channel.invokeMethod('setConversationListTitle', title);
   }
 
+  /// Enables or disables the FAQs/the Knowledge base using the value provided for [visibility].
   static Future<Null> setFAQVisibility(bool visibility) async {
     await _channel.invokeMethod('setFAQVisibility', visibility);
   }
 
+  /// Registers a visitor using the unique ID provided for [registerID].
+  /// Once registered, conversations may be restored and synced across multiple devices that are registered with the same [registerID].
+  /// Use the API during a `login` operation to set the user's session.
   static Future<dynamic> registerVisitor(String registerID) async {
     return await _channel.invokeMethod('registerVisitor', registerID);
   }
 
+  /// Unregisters and clears conversations and data for the current user.
+  /// Use the API during a `logout` operation to clear data.
   static Future<Null> unregisterVisitor() async {
     await _channel.invokeMethod('unregisterVisitor');
   }
 
+  /// Sets the current page title to be shown in the visitor footpath on the SalesIQ console.
   static Future<Null> setPageTitle(String pageTitle) async {
     await _channel.invokeMethod('setPageTitle', pageTitle);
   }
 
+  /// Performs a custom action using the action name provided in [actionName].
   static Future<Null> performCustomAction(String actionName) async {
     await _channel.invokeMethod('performCustomAction', actionName);
   }
 
+  /// Enables in-app notifications from Mobilisten if previously disabled.
+  /// In-app notifications are `enabled` by default.
   static Future<Null> enableInAppNotification() async {
     await _channel.invokeMethod('enableInAppNotification');
   }
 
+  /// Disables in-app notifications from Mobilisten.
   static Future<Null> disableInAppNotification() async {
     await _channel.invokeMethod('disableInAppNotification');
   }
 
+  /// Sets the operator to whom all chat requests need to be routed using the provided [email].
   static Future<Null> setOperatorEmail(String email) async {
     await _channel.invokeMethod('setOperatorEmail', email);
   }
 
+  /// Opens the Mobilisten UI. Invoke this API only after initialization is complete.
   static Future<Null> show() async {
     await _channel.invokeMethod('show');
   }
 
+  /// Opens the chat window for a specified chat if provided the [chatID].
   static Future<Null> openChatWithID(String chatID) async {
     await _channel.invokeMethod('openChatWithID', chatID);
   }
 
+  /// Opens a new chat window for creating a new chat.
   static Future<Null> openNewChat() async {
     await _channel.invokeMethod('openNewChat');
   }
 
+  /// Shows an offline banner if all departments are offline.
+  /// This API is indended for use only when then chat waiting time is set to `Infinite`.
   static Future<Null> showOfflineMessage(bool show) async {
     await _channel.invokeMethod('showOfflineMessage', show);
   }
 
+  /// Ends the specified chat if provided the [chatID].
   static Future<Null> endChat(String chatID) async {
     await _channel.invokeMethod('endChat', chatID);
   }
 
+  /// Sets the visitor's name.
   static Future<Null> setVisitorName(String visitorName) async {
     await _channel.invokeMethod('setVisitorName', visitorName);
   }
 
+  /// Sets the visitor's email.
   static Future<Null> setVisitorEmail(String visitorEmail) async {
     await _channel.invokeMethod('setVisitorEmail', visitorEmail);
   }
 
+  /// Sets the visitor's contact number.
   static Future<Null> setVisitorContactNumber(String contactNumber) async {
     await _channel.invokeMethod('setVisitorContactNumber', contactNumber);
   }
 
+  /// Sets the visitor's custom information as [key], [value] pairs.
   static Future<Null> setVisitorAddInfo(String key, String value) async {
     Map<String, dynamic> addInfo = <String, dynamic>{};
     addInfo.putIfAbsent("key", () => key);
@@ -127,6 +163,7 @@ class ZohoSalesIQ {
     await _channel.invokeMethod('setVisitorAddInfo', addInfo);
   }
 
+  /// Sets the visitor's secondary location.
   static Future<Null> setVisitorLocation(
       SIQVisitorLocation locationDetails) async {
     Map<String, dynamic> location = <String, dynamic>{};
@@ -140,66 +177,82 @@ class ZohoSalesIQ {
     await _channel.invokeMethod('setVisitorLocation', location);
   }
 
+  /// Sets the title displayed in the chat window prior to starting a conversation.
   static Future<Null> setChatTitle(String chatTitle) async {
     await _channel.invokeMethod('setChatTitle', chatTitle);
   }
 
+  /// Sets the overall theme color used in the iOS platform.
   static Future<Null> setThemeColorForiOS(String hexColor) async {
     await _channel.invokeMethod('setThemeColorForiOS', hexColor);
   }
 
+  /// Enables showing the operator's image in the default launcher.
   static Future<Null> showOperatorImageInLauncher(bool show) async {
     await _channel.invokeMethod('showOperatorImageInLauncher', show);
   }
 
+  /// Enables or disables the display of sender images for incoming messages based on the value provided for [show].
   static Future<Null> showOperatorImageInChat(bool show) async {
     await _channel.invokeMethod('showOperatorImageInChat', show);
   }
 
+  /// Enables or disables showing the visitor name _if available_ as the sender name for outgoing messages within chat.
   static Future<Null> setVisitorNameVisibility(bool visibility) async {
     await _channel.invokeMethod('setVisitorNameVisibility', visibility);
   }
 
+  /// Enables or disables the option to provide feedback for a chat once ended.
   static Future<Null> setFeedbackVisibility(bool visibility) async {
     await _channel.invokeMethod('setFeedbackVisibility', visibility);
   }
 
+  /// Enables or disables the option to provide rating for a chat once ended.
   static Future<Null> setRatingVisibility(bool visibility) async {
     await _channel.invokeMethod('setRatingVisibility', visibility);
   }
 
+  /// Enables the option to capture screenshots from the attachments menu.
   static Future<Null> enableScreenshotOption() async {
     await _channel.invokeMethod('enableScreenshotOption');
   }
 
+  /// Disables the option to capture screenshots fromm the attachments menu.
   static Future<Null> disableScreenshotOption() async {
     await _channel.invokeMethod('disableScreenshotOption');
   }
 
+  /// Enables the pre-chat form if previously disables. Pre-chat forms are `enabled` by default.
   static Future<Null> enablePreChatForms() async {
     await _channel.invokeMethod('enablePreChatForms');
   }
 
+  /// Disables the pre-chat form.
   static Future<Null> disablePreChatForms() async {
     await _channel.invokeMethod('disablePreChatForms');
   }
 
+  /// Returns a list of chats (Instances of [SIQChat]).
   static Future<List<SIQChat>> getChats() async {
     final List mapList = await _channel.invokeMethod('getChats');
     return _getChatObjectList(mapList);
   }
 
-  static Future<List<SIQChat>> getChatsWithFilter(SIQChatStatus filter) async {
+  /// Returns a list of chats (Instances of [SIQChat]) whose status matches [chatStatus].
+  static Future<List<SIQChat>> getChatsWithFilter(
+      SIQChatStatus chatStatus) async {
     final List mapList = await _channel.invokeMethod(
-        'getChatsWithFilter', filter.toShortString());
+        'getChatsWithFilter', chatStatus.toShortString());
     return _getChatObjectList(mapList);
   }
 
+  /// Returns a list of articles (Instances of [SIQArticle]).
   static Future<List<SIQArticle>> getArticles() async {
     final List articleList = await _channel.invokeMethod('getArticles');
     return _getArticleObjectList(articleList);
   }
 
+  /// Returns a list of articles (Instances of [SIQArticle]) belonging to a specific category if provided a [categoryID].
   static Future<List<SIQArticle>> getArticlesWithCategoryID(
       String categoryID) async {
     final List articleList =
@@ -207,17 +260,20 @@ class ZohoSalesIQ {
     return _getArticleObjectList(articleList);
   }
 
+  /// Returns a list of article categories (Instances of [SIQArticleCategory]).
   static Future<List<SIQArticleCategory>> getArticleCategories() async {
     final List categoryList =
         await _channel.invokeMethod('getArticleCategories');
     return _getArticleCategoryObjectList(categoryList);
   }
 
+  /// Returns a list of departments (Instances of [SIQDepartment]).
   static Future<List<SIQDepartment>> getDepartments() async {
     final List deptList = await _channel.invokeMethod('getDepartments');
     return _getDepartmentObjectList(deptList);
   }
 
+  /// Returns the base64 representation of an attender image for the given attender ID.
   static Future<String> fetchAttenderImage(
       String attenderID, bool fetchDefaultImage) async {
     Map<String, dynamic> details = <String, dynamic>{};
@@ -228,32 +284,40 @@ class ZohoSalesIQ {
     return image;
   }
 
+  /// Opens an article from the knowledge base using the [articleID].
   static Future<String> openArticle(String articleID) async {
     final String articleList =
         await _channel.invokeMethod('openArticle', articleID);
     return articleList;
   }
 
+  /// Registers a chat action to be used in display-cards using an action name.
   static Future<Null> registerChatAction(String actionName) async {
     await _channel.invokeMethod('registerChatAction', actionName);
   }
 
+  /// Unregisters a chat action to be used in display-cards using an action name.
   static Future<Null> unregisterChatAction(String actionName) async {
     await _channel.invokeMethod('unregisterChatAction', actionName);
   }
 
+  /// Unregisters all registered chat action to be used in display-cards.
   static Future<Null> unregisterAllChatActions() async {
     await _channel.invokeMethod('unregisterAllChatActions');
   }
 
+  /// Sets the timeout for all chat actions.
   static Future<Null> setChatActionTimeout(int timeout) async {
     await _channel.invokeMethod('setChatActionTimeout', timeout);
   }
 
+  /// Marks a chat action as complete provided the [actionUUID].
   static Future<Null> completeChatAction(String actionUUID) async {
     await _channel.invokeMethod('completeChatAction', actionUUID);
   }
 
+  /// Enables push notifications for iOS using [token], [isTestDevice] and [productionMode].
+  /// Set [isTestDevice] to `false` and [productionMode] to `true` before moving the app to production.
   static Future<Null> enablePushForiOS(
       String token, bool isTestDevice, bool productionMode) async {
     Map<String, dynamic> args = <String, dynamic>{};
@@ -263,14 +327,19 @@ class ZohoSalesIQ {
     await _channel.invokeMethod('enablePushForiOS', args);
   }
 
+  /// Processes the content of push notifications in response to a the tap action in iOS.
+  /// Use this API only if push notification configuration is to be done manually in dart.
   static Future<Null> handleNotificationResponseForiOS(Map userInfo) async {
     await _channel.invokeMethod('handleNotificationResponseForiOS', userInfo);
   }
 
+  /// Processes the content of push notification received for iOS.
+  /// Use this API only if push notification configuration is to be done manually in dart.
   static Future<Null> processNotificationWithInfoForiOS(Map userInfo) async {
     await _channel.invokeMethod('processNotificationWithInfoForiOS', userInfo);
   }
 
+  /// Marks a chat action as complete provided the [actionUUID], completion state and the message to be shown upon completion.
   static Future<Null> completeChatActionWithMessage(
       String actionUUID, bool state, String message) async {
     Map<String, dynamic> chatActionDetails = <String, dynamic>{};
@@ -281,6 +350,7 @@ class ZohoSalesIQ {
         'completeChatActionWithMessage', chatActionDetails);
   }
 
+  /// A Boolean value used to determine whether a visitor can start multiple parallely open chats.
   static Future<bool> get isMultipleOpenChatRestricted async {
     return await _channel.invokeMethod('isMultipleOpenChatRestricted');
   }
